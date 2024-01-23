e3c289cc4e5feb5e5657498a8e24199058de535f
itxcrusher <muhammadhassaanjaved99@gmail.com>
2024-01-23 15:44:27 +0500

    Shell Script

--- Lab/task.sh
+++ Lab/task.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 
 # Stop and remove all containers
-sudo docker stop $(docker ps -aq)
-sudo docker rm $(docker ps -aq)
+sudo docker ps -a | grep "app:latest" | awk '{print $1}' | xargs -r sudo docker stop
+#sudo docker stop $(docker ps -aq)
+sudo docker ps -a | grep "app:latest" | awk '{print $1}' | xargs -r sudo docker rm
+#sudo docker rm $(docker ps -aq)
 
 # Remove all images
-sudo docker rmi $(docker images -aq)
+sudo docker rmi app
+#sudo docker rmi $(docker images -aq)
 
 # Build a new Docker image from the Dockerfile
 sudo docker build -t app:latest ../flaskApp/
