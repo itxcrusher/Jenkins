4d37e50f4ecb175aacbf1d29441299b8ca210a41
itxcrusher <muhammadhassaanjaved99@gmail.com>
2024-01-23 19:42:58 +0500

    Updated

--- Lab/app.py
+++ Lab/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<br><center><h1>Flask Application using Jenkins pipeline!</h1></center>'
+    return '<br><center><h1>Flask Application test using Jenkins pipeline!</h1></center>'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
